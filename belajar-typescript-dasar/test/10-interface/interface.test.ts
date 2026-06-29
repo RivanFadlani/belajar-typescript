43f6911278c6c83a89f3376fe765ef2fcb9f0d80
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-29 13:57:42 +0700

    study: Function Interface (290626)

--- belajar-typescript-dasar/test/10-interface/interface.test.ts
+++ belajar-typescript-dasar/test/10-interface/interface.test.ts
@@ -1,7 +1,8 @@
-import { describe, it } from "vitest";
+import { describe, it, expect } from "vitest";
 import type { Seller } from "../../src/10-interface/interface";
 
 describe("Interface", function () {
+  // Interface
   it("should support Interface in typescript", function () {
     const seller: Seller = {
       id: 1,
@@ -16,5 +17,21 @@ describe("Interface", function () {
     // seller.nib = "892134922" * tidak bisa diubah karena property bersifat read-only
 
     console.log(seller);
+  });
+
+  // Function Interface
+  it("should support function interface in typescript", function () {
+    interface AddFunction {
+      //         parameter      ----    return
+      (value1: number, value2: number): number;
+    }
+
+    const add: AddFunction = (value1: number, value2: number) => {
+      return value1 + value2;
+    };
+    // kalau di javascript sama saja seperti ini:
+    // const add = () => { return value1 + value2 }
+
+    expect(add(2, 2)).toBe(4);
   });
 });
