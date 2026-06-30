a05274cd5b6d8239b28d1a77b391038f6d6c29b3
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-30 11:51:07 +0700

    study: Properties or Field (300626)

---
+++ belajar-typescript-oop/test/02-properties/properties.test.ts
@@ -0,0 +1,27 @@
+describe("Properties", function () {
+  class Customer {
+    // Property atau bisa disebut Field
+    readonly id: number; // readonly
+    name: string; // mandatory
+    age?: number; // optional property
+    rank: string = "Silver"; // default value
+    // rank: sama seperti optional property, default value tidak perlu dipanggil di parameter
+
+    // constructor menerima argument dari new instance
+    constructor(id: number, name: string) {
+      this.id = id; // mengacu pada field di luar scope constructor
+      this.name = name; // * linked ke field/property 'name'
+    }
+  }
+
+  it("should can have properties", function () {
+    const customer: Customer = new Customer(1, "ripunn");
+    customer.age = 18;
+
+    console.log(customer);
+    console.log("id:", customer.id);
+    console.log("name:", customer.name);
+    console.log("age:", customer.age);
+    console.log("default value:", customer.rank);
+  });
+});
