a7ff857614b9a985cad01fbc911a8b86a7d471fe
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-30 16:59:00 +0700

    study: Method Overriding (300626)

---
+++ belajar-typescript-oop/test/07-method-overriding/method-overriding.test.ts
@@ -0,0 +1,30 @@
+describe("Method Overriding", function () {
+  class Employee {
+    name: string;
+
+    constructor(name: string) {
+      this.name = name;
+    }
+
+    sayHello(name: string): void {
+      console.log(`Hello ${name}! my name is ${this.name}`);
+    }
+  }
+
+  class Manager extends Employee {
+    // overriding method dari employee
+    sayHello(name: string): void {
+      console.log(`Weh! mas ${name} timpa teks punya ${this.name} loh ya xD`);
+    }
+  }
+
+  it("should support method overriding", function () {
+    const employee = new Employee("Rivan");
+    employee.sayHello("ripunn");
+
+    const manager = new Manager("Frieren");
+    manager.sayHello("Fern");
+
+    // hasilnya, setiap hasil log sayHello ditiap class akan berbeda-beda, mau Employee ataupun Manager
+  });
+});
