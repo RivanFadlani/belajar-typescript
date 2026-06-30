13e0f50c158d84b85bf61dd4e446fbac5da1be73
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-30 10:56:21 +0700

    study: Constructor (300626)

--- belajar-typescript-oop/test/01-class/class.test.ts
+++ belajar-typescript-oop/test/01-class/class.test.ts
@@ -1,10 +1,28 @@
 describe("Class", function () {
+  class Customer {
+    // constructor itu seperti function dia memiliki parameter yang bisa diisi
+    constructor() {
+      console.log("Create New Customer");
+    }
+  }
+
+  class Order {
+    constructor() {
+      console.log("aku mau mie ayam 3");
+    }
+  }
+
+  // Class
   it("should can create class", function () {
-    // * class Customer {}
-    // * class Order {}
     // Explicit, tipe datanya adalah 'Customer'
     // * const customer: Customer = new Customer();
     // Implicit, biarkan typescript cari tahu sendiri
     // * const order = new Order();
+  });
+
+  // Constructor
+  it("should can create constructor", function () {
+    new Customer(); // memanggil constructor pada class Customer
+    new Order();
   });
 });
