4e0fc761ebbd2916264b9214f71c40c801b30631
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-30 10:38:56 +0700

    study: Class (300626)

---
+++ belajar-typescript-oop/test/01-class/class.test.ts
@@ -0,0 +1,10 @@
+describe("Class", function () {
+  it("should can create class", function () {
+    // * class Customer {}
+    // * class Order {}
+    // Explicit, tipe datanya adalah 'Customer'
+    // * const customer: Customer = new Customer();
+    // Implicit, biarkan typescript cari tahu sendiri
+    // * const order = new Order();
+  });
+});
