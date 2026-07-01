300bb9f7c5cab65b09453b1589febadbb878083b
RivanFadlani <rivanfadlani7@gmail.com>
2026-07-01 15:45:01 +0700

    study: Optional Generic Type (010726)

--- belajar-typescript-generic/test/04-multiple-generic-type/multiple-generic-type.test.ts
+++ belajar-typescript-generic/test/04-multiple-generic-type/multiple-generic-type.test.ts
@@ -31,4 +31,43 @@ describe("Multiple Generic Type", () => {
     expect(triple.value).toBe("ripunn");
     expect(triple.third).toBe(true);
   });
+
+  // Optional Generic Type
+  it("should support optional generic type", () => {
+    /*
+     * generic type mendukung agar field yang ada generic type pada class tidak perlu explicit disebut data type-nya
+     * ketika membuat instance object
+     * */
+    // selama field/property ada di dalam constructor
+
+    // tidak perlu explicit sebutkan 'new Entry<number, string>()'
+    const entry = new Entry(1, "ripunn");
+    expect(entry.key).toBe(1);
+    expect(entry.value).toBe("ripunn");
+  });
+
+  class simpleGeneric<T> {
+    private value?: T;
+
+    getValue(): T | undefined {
+      return this.value;
+    }
+
+    setValue(value: T) {
+      this.value = value;
+    }
+  }
+
+  it("should have problem non-constructor generic type", () => {
+    // jika tipe data tidak dideklarasikan pada instance object, maka tipe data akan sama saja seperti 'Any'
+    const simple = new simpleGeneric<string>();
+    // di bawah ini akan diproses semua ke expect
+    // karena ada method toUpperCase(). maka expect hanya menerima string saja
+    // jadi kita perlu deklarasi tipe data pada generic untuk memfilter hanya yang tipe data string saja yang bisa diproses
+    simple.setValue("ripunn");
+    // simple.setValue(100);
+    // simple.setValue(true);
+
+    expect(simple.getValue()!.toUpperCase()).toBe("RIPUNN");
+  });
 });
